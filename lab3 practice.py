18cc17130ac5c00c131d073ae01639b495e0a5c3
zunairarehman <107191132+zunairarehman@users.noreply.github.com>
2024-11-16 12:00:06 +0500

    lab3 practice.py

--- lab3 practice.py
+++ lab3 practice.py
@@ -1,19 +1,19 @@
-#addition of strings
+#program 1
 first_number = str(input("Enter first number:"))
 second_number = str(input("Enter second number:"))
 sum = (first_number + second_number)
 print("The addition of two numbers is:", sum)
-#seperator
+#program 2
 a=5
 print("a=", a, sep='0', end=',\n')
-#employee details
+#program 3
 name= input ("Enter employee name:")
 salary= input ("Enter salary:")
 company= input ("Enter company name:")
 print("PRINTING EMPLOYEE DETAILS")
 print("NAME","SALARY","COMPANY")
 print(name, salary, company)
-#bio-data
+#program 4
 name=input("Enter your name:")
 dob=input("Enter date of birth:")
 rollno=input("Enter your roll no:")
@@ -24,11 +24,11 @@ per1=input("Enter your intermediate percentage:")
 grade1=input("Enter your intermediate grade:")
 print("\t***BIO-DATA***")
 print(name,"\n",dob,"\n",rollno,"\n",section,"\n",per,"\n",grade,"\n",per1,"\n",grade1)
-#using .upper()
+#program 5
 print("What kind of food would you like?")
 food=input()
 print("Let me find the best",food.upper(),"for you")
-#marksheet
+#program 6
 course1,course2,course3,course4,course5=eval(input("Enter marks of 5 subjects:"))
 total=course1+course2+course3+course4+course5
 average=total/5
